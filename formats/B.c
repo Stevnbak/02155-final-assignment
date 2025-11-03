f9c23f0dfcfc409f6ca9214e6a37ef4c1ced8b54
MartinKeithBuchreitz <88722233+MartinKeithBuchreitz@users.noreply.github.com>
2025-11-03 16:18:00 +0100

    branches

--- formats/B.c
+++ formats/B.c
@@ -1,5 +1,5 @@
 #include <stdint.h>
-
+#include "../cpu.h"
 int executeBFormat(
     uint8_t rd,
     uint8_t rs1,
@@ -7,5 +7,43 @@ int executeBFormat(
     uint8_t funct3,
     uint8_t funct7
 ) {
+    
+    
+    uint16_t imm_temp = (((uint16_t) funct7 << 7) + rd) << 1;
+    uint16_t imm = (imm_temp & 0x1000 >> 1) + ((imm_temp & 0x7E0)) + ((imm_temp & 0x3E)) + ((imm_temp & 0x800) >> 11);
+    switch (funct3){
+        case 0x0: // beq
+            if (getRegister(rs1) == getRegister(rs2)){
+                PC += imm;
+            }
+            break;
+        case 0x1: // bne
+            if (getRegister(rs1) != getRegister(rs2)){
+                PC += imm;
+            }
+            break;
+        case 0x4: // blt
+            if (getRegister(rs1) < getRegister(rs2)){
+                PC += imm;
+            }
+            break;
+        case 0x5: // bge
+            if (getRegister(rs1) >= getRegister(rs2)){
+                PC += imm;
+            }
+            break;
+        case 0x6: // bltu
+            if (getRegister(rs1) < getRegister(rs2)){
+                PC += imm;
+            }
+            break;
+        case 0x7: // bgeu
+            if (getRegister(rs1) <= getRegister(rs2)){
+                PC += imm;
+            }
+            break;
+        default:
+            return -1;
+    }
     return 0;
 }
