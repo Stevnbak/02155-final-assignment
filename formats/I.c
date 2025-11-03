294a00bea9f45e01c0bdbe9f7070336b1ecc46f2
Stevnbak <kasper@stevnbak.dk>
2025-11-03 14:46:52 +0100

    I instruction opcodes

--- formats/I.c
+++ formats/I.c
@@ -8,5 +8,15 @@ int executeIFormat(
     uint8_t funct3,
     uint8_t funct7
 ) {
+    switch(opcode) {
+        case 0x13: //0010011
+            break;
+        case 0x3:  //0000011
+            break;
+        case 0x67: //1100111
+            break;
+        case 0x73: //1110011
+            break;
+    }
     return 0;
 }
