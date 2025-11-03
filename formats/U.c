c3382075586f1c84272ff1a4d7f1695e69861c97
MartinKeithBuchreitz <88722233+MartinKeithBuchreitz@users.noreply.github.com>
2025-11-03 16:24:20 +0100

    U done

--- formats/U.c
+++ formats/U.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include "../cpu.h"
 
 int executeUFormat(
     uint8_t opcode,
@@ -8,5 +9,16 @@ int executeUFormat(
     uint8_t funct3,
     uint8_t funct7
 ) {
+    uint32_t imm = (((uint32_t)funct7) << 13) + (((uint32_t)rs2) << 8) + (((uint32_t)rs1) << 3) + (((uint32_t)funct3));
+    switch(opcode){
+        case 0x37: 
+            setRegister(rd, (imm << 12));
+            break;
+        case 0x17: 
+            PC += (imm << 12);
+            break;
+        default:
+            return -1;
+    }
     return 0;
 }
