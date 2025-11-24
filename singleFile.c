c76e6a148337bed1017c219c7b64187b728d8386
Stevnbak <kasper@stevnbak.dk>
2025-11-24 13:57:28 +0100

    Make output .res file

--- singleFile.c
+++ singleFile.c
@@ -1,8 +1,16 @@
 #include "src/program.h"
+#include "src/cpu.h"
+
+#include <stdio.h>
 
 // Execute single program
 int main(int argc, char* argv[]) {
     if(argc < 2) return 1;
     executeProgram(argv[1], 1);
-    reset();
+    FILE* output = fopen("output.res", "wb");
+    for(uint8_t i = 0; i < 32; i++) {
+        uint32_t v =  getRegisterUnsigned(i);
+        fwrite(&v, 1, sizeof(uint32_t), output);
+    }
+    fclose(output);
 }
